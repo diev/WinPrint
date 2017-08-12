986154683db9161cbb9757d6ae6cd5c3f57faadf
Dmitrii Evdokimov <diev@mail.ru>
2017-08-12 06:16:57 +0300

    Add regexp for KA

--- PrintUnit.pas
+++ PrintUnit.pas
@@ -7,6 +7,7 @@ uses
 
 procedure LoadLines(const FileName: string; const Enc: Integer = 866);
 procedure PrintLines;
+function TryGetKA(const S: string; out KA: string): Boolean;
 
 var
   Lines: TStrings;
@@ -14,23 +15,51 @@ var
 implementation
 
 uses
-  System.SysUtils;
+  System.SysUtils,
+  System.RegularExpressions;
 
 procedure LoadLines(const FileName: string; const Enc: Integer = 866);
 begin
-  Lines := TStringList.Create();
   Lines.LoadFromFile(FileName, TEncoding.GetEncoding(Enc));
 end;
 
 procedure PrintLines;
 var
   I: Integer;
+  S: string;
+  Fin: Boolean;
+  KA: string;
 begin
+  Fin := false;
   for I := 0 to Lines.Count - 1 do
   begin
-    Writeln(Lines[I]);
+    S := TrimRight(Lines[I]);
+    if S.StartsWith('o000000') then
+      Fin := true;
+    if Fin then
+    begin
+      if TryGetKA(S, KA) then
+        Writeln(Format('[KA %s]', [KA]));
+    end
+    else
+      Writeln(S);
   end;
 end;
 
+function TryGetKA(const S: string; out KA: string): Boolean;
+var
+  res: TMatch;
+begin
+  res := TRegEx.Match(S, '(\d{12})');
+  Result := res.Success;
+  if Result then
+    KA := res.Value;
+end;
+
+initialization
+  Lines := TStringList.Create;
+
+finalization
+  Lines.Free;
 end.
 
